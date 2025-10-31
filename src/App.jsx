753c2470acb01dab270b9a8e7ff41a0e8756df0f
foretechleaders <foretechleaders@gmail.com>
2025-10-31 10:37:53 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-[url('/sand-dune.jpg')] bg-cover bg-center bg-fixed text-slate-900 flex flex-col">
-      <div className="bg-white/80 backdrop-blur-sm flex-1 flex flex-col">
+    // Global animated golden-beige background (except homepage hero)
+    <div className="min-h-screen bg-animated-sand text-slate-900 flex flex-col">
+      <div className="flex-1 flex flex-col">
         <Navbar />
         <main className="flex-1">
           <Outlet />
